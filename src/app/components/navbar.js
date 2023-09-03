4def9c32003783bbacfb22f12bc25dde29416637
Arely Rivera <arelyrivera1996@gmail.com>
2023-09-02 23:10:51 -0700

    added icons in nav

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,41 +1,46 @@
-import Image from 'next/image';
-
 const navPaths = [
   {
     name: "Home",
     path: "#home",
     icon: "/assets/icons/house.svg",
+    alt: "house",
   },
   {
     name: "Projects",
     path: "#projects",
     icon: "/assets/icons/code.png",
+    alt: "laptop with code symbol",
   },
   {
     name: "About",
     path: "#about",
     icon: "/assets/icons/user.png",
+    alt: "user",
   },
   {
     name: "Resume",
     path: "#resume",
     icon: "/assets/icons/document.png",
+    alt: "document",
   },
   {
     name: "Contact",
     path: "#contact",
     icon: "/assets/icons/mail.png",
+    alt: "envelop",
   },
 ];
 
 export default function Navbar() {
   return (
     <nav>
-      <ul>
+      <ul className="mt-3 ml-3">
         {navPaths.map((item, index) => (
-          <li key={index}>
-            <a href={item.path}>{item.name}</a>
-            <img src={item.icon} />
+          <li key={index} className="mb-3 ">
+            <a href={item.path} className="flex items-center">
+              {item.name}
+              <img src={item.icon} alt={item.alt} className="ml-2" />
+            </a>
           </li>
         ))}
       </ul>
